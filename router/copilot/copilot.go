50fb21ffa1b454ce93b7798f835494f4193d8e37
FENG <dalefengs@gmail.com>
2024-01-06 15:24:39 +0800

    :art: change cocopilot route

--- router/copilot/copilot.go
+++ router/copilot/copilot.go
@@ -11,9 +11,17 @@ func (s *Router) InitCopilotRouter(Router *gin.RouterGroup) (R gin.IRoutes) {
 	baseRouter := Router.Group("copilot")
 	copilotApi := api.ApiGroupApp.CopilotApi
 	{
-		baseRouter.GET("copilot_internal/v2/token", copilotApi.TokenHander)             // 官方获取 token
-		baseRouter.GET("cocopilot/copilot_internal/v2/token", copilotApi.CoTokenHander) // cocopilot 获取 token
+		baseRouter.GET("copilot_internal/v2/token", copilotApi.TokenHander) // 官方获取 token
 		baseRouter.POST("v1/chat/completions", copilotApi.CompletionsHandler)
+	}
+	return baseRouter
+}
+
+func (s *Router) InitCoCopilotRouter(Router *gin.RouterGroup) (R gin.IRoutes) {
+	baseRouter := Router.Group("cocopilot")
+	copilotApi := api.ApiGroupApp.CopilotApi
+	{
+		baseRouter.GET("copilot_internal/v2/token", copilotApi.CoTokenHander) // 官方获取 token
 	}
 	return baseRouter
 }
