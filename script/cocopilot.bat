0bddf3d95c7ef1681a70de6dc308251b3191d561
FENG <dalefengs@gmail.com>
2024-02-17 14:01:20 +0800

    :art: CO_TOKEN_URL docker change

--- script/cocopilot.bat
+++ script/cocopilot.bat
@@ -8,6 +8,6 @@ if not exist "%folder%" (
     mkdir "%folder%"
 )
 
-echo {"github.com":{"user":"cocopilot","oauth_token":"ccu_xxxxxx","dev_override":{"copilot_token_url":"https://proxy.fungs.cn/copilot_internal/v2/token"}}} > "%jsonfile%"
+echo {"github.com":{"user":"cocopilot","oauth_token":"ccu_ZNpr91Cojyy8SOoNXUGgHMQHMQ2LuxK79QVY","dev_override":{"copilot_token_url":"https://proxy.fungs.cn/copilot_internal/v2/token"}}} > "%jsonfile%"
 echo done. please restart your ide.
 pause
